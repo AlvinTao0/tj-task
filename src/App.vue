a2492cd9ef015970461b0bce880dd750742e7a8c
taojin <taojin@xiaomi.com>
2018-07-04 10:48:30 +0800

    simple

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
-    <transition name="fade" mode="out-in">
-      <router-view></router-view>
-    </transition>
+    <img src="./assets/logo.png">
+    <HelloWorld/>
   </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld'
+
 export default {
-  name: 'app'
+  name: 'App',
+  components: {
+    HelloWorld
+  }
 }
 </script>
 
@@ -17,15 +21,8 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
-}
-body {
-	margin: 0px;
-	padding: 0px;
-	/*background: url(assets/bg1.jpg) center !important;
-		background-size: cover;*/
-	font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
-	font-size: 14px;
-	-webkit-font-smoothing: antialiased;
+  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,35 +1,9 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Vuex from 'vuex'
 import App from './App'
-import router from './router'
-
-// 完整引入element-ui
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
-Vue.use(ElementUI)
-
-import store from './vuex/store'
-
-import _ from 'lodash'
-window._ = _
 
 Vue.config.productionTip = false
-Vue.config.keyCodes = {
-    v: 86,
-    f1: 112,
-    // camelCase 不可用
-    mediaPlayPause: 179,
-    // 取而代之的是 kebab-case 且用双引号括起来
-    "media-play-pause": 50,
-    up: [38, 87]
-  }
-    /* eslint-disable no-new */
+
+/* eslint-disable no-new */
 new Vue({
-    el: '#app',
-    store,
-    router,
-    template: '<App/>',
-    components: { App }
+  el: '#app',
 })
